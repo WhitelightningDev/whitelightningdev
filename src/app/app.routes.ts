929e3820ea00d5ec47a5c38d04d270033db3035e
Daniel <danielmommsen2@gmail.com>
2025-03-23 11:06:20 +0200

    changed the interface of the website

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { PortfolioComponent } from './pages/portfolio/portfolio.component';
 
 export const routes: Routes = [
 
   // Page links
   {path: "", component: HomeComponent},
-  {path: "about", component: AboutComponent}
+  {path: "about", component: AboutComponent},
+  {path: "portfolio", component: PortfolioComponent}
 ];
